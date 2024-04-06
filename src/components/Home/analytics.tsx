225236a78ceb32fcf44b2065d5a5579706921d8e
milankatira26 <milan@instaservice.com>
2024-04-06 09:04:13 +0530

    added dashboard things

---
+++ src/components/Home/analytics.tsx
@@ -0,0 +1,57 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+const Analytics = () => {
+
+    return (
+        <div className="w-full p-4 flex flex-row gap-4 justify-between">
+        <Card className='w-full'>
+        <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+            <p>Card Footer</p>
+        </CardFooter>
+            </Card>
+
+            <Card className='w-full'>
+        <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+            <p>Card Footer</p>
+        </CardFooter>
+            </Card>
+
+            <Card className='w-full'>
+        <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+            <p>Card Footer</p>
+        </CardFooter>
+            </Card>
+        </div>
+
+    )
+}
+
+export default Analytics;
